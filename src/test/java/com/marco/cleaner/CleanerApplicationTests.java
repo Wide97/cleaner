68cd79ae0e564ed10c0b031d30d0fb02d0fc0698
Wide97 <mwidesott@gmail.com>
2025-03-31 00:12:43 +0200

    Init project

---
+++ src/test/java/com/marco/cleaner/CleanerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marco.cleaner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
